428df9ea75c80f7267bf9aa1a58397306bea9f52
Muntakim Islam Idrak <43186205+idrak07@users.noreply.github.com>
2023-01-29 22:58:27 +0600

    Revert "Revert "changed""
    This reverts commit ae0c12e6fe1f654272e7d6a4f6eb510cd73f8491.

--- spring-backend/src/main/java/com/genuinecoder/springserver/repository/UserAuthoritiesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.genuinecoder.springserver.repository;
-
-import com.genuinecoder.springserver.domain.UserAuthority;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserAuthoritiesRepository extends JpaRepository<UserAuthority, Long> {
-}
