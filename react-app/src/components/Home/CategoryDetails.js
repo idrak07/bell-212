1c4405e2a7c4d52c3753bfc9870feef09a9b6aaf
Muntakim Islam Idrak <43186205+idrak07@users.noreply.github.com>
2023-02-01 01:59:40 +0600

    css fixed

--- react-app/src/components/Home/CategoryDetails.js
+++ react-app/src/components/Home/CategoryDetails.js
@@ -3,78 +3,82 @@ import Navbars from './Navbars';
 import Footer from './Footer'
 import Card from 'react-bootstrap/Card';
 import CardGroup from 'react-bootstrap/CardGroup';
+import engine from '../image/engine0.png';
+import airframe from '../image/airframe.png';
+import radio from '../image/radio.jpg';
 import air from '../image/aircraft.jpg';
 import unit from '../image/unit.jpg';
-import occean from '../image/one.webp';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const CategooryDetails = () => {
     return (
         <div>
-           <Navbars></Navbars>  
-           
+           <Navbars></Navbars>
+           <br/>
+           <br/>
            <div className="container">
-    <CardGroup>
-      <Card>
-      <Link to="/overview">
-        <Card.Img variant="top" src={air}/>
-        <Card.Body>
-          <Card.Title>Engine</Card.Title>
-          <Card.Text>
-            Engine Related tutorial will be found here
-          </Card.Text>
-        </Card.Body>
-        </Link>
-      </Card>
-      <Card>
-      <Link to="/aircond">
-        <Card.Img variant="top" src={unit} />
-        <Card.Body>
-          <Card.Title>Air Craft</Card.Title>
-          <Card.Text>
-          Air Craft Related tutorial will be found here
-          </Card.Text>
-        </Card.Body>
-        </Link> 
-      </Card>
-      <Card>
-      <Link to="/radoverview">
-        <Card.Img variant="top" src={occean} />
-        <Card.Body>
-          <Card.Title>Radio</Card.Title>
-          <Card.Text>
-          Radio Related tutorial will be found here
-          </Card.Text>
-        </Card.Body>
-        </Link>   
-      </Card>
-    </CardGroup>
-    <CardGroup>
-      <Card>
-      <Link to="/elecoverview">
-        <Card.Img variant="top" src={air}/>
-        <Card.Body>
-          <Card.Title>Electrical</Card.Title>
-          <Card.Text>
-          Electrical Related tutorial will be found here
-          </Card.Text>
-        </Card.Body>
-        </Link>
-      </Card>
-      <Card>
-      <Link to="/instoverview">
-        <Card.Img variant="top" src={unit} />
-        <Card.Body>
-          <Card.Title>Instruments</Card.Title>
-          <Card.Text>
-          Instruments Related tutorial will be found here
-          </Card.Text>
-        </Card.Body>
-        </Link> 
-      </Card>
-    </CardGroup>
-    </div>
-    <Footer></Footer>
+              <CardGroup>
+                <Card className='mx-2 card-hover'>
+                  <Link to="/overview" style={{ textDecoration: 'none', color: 'black' }}>
+                    <Card.Img variant="top" src={engine}/>
+                    <Card.Body>
+                      <Card.Title><b>Engine</b></Card.Title>
+                      <Card.Text className='py-2'>
+                        Engine Related tutorial will be found here
+                      </Card.Text>
+                    </Card.Body>
+                  </Link>
+                </Card>
+                <Card className='mx-2 card-hover'>
+                  <Link to="/aircond" style={{ textDecoration: 'none', color: 'black' }}>
+                    <Card.Img variant="top" src={airframe} />
+                    <Card.Body>
+                      <Card.Title><b>Air Frame</b></Card.Title>
+                      <Card.Text className='py-2'>
+                      Air Craft Related tutorial will be found here
+                      </Card.Text>
+                    </Card.Body>
+                  </Link> 
+                </Card>
+                <Card className='mx-2 card-hover'>
+                  <Link to="/radoverview" style={{ textDecoration: 'none', color: 'black' }}>
+                    <Card.Img variant="top" src={radio} />
+                    <Card.Body>
+                      <Card.Title><b>Radio</b></Card.Title>
+                      <Card.Text className='py-2'>
+                      Radio Related tutorial will be found here
+                      </Card.Text>
+                    </Card.Body>
+                  </Link>   
+                </Card>
+              </CardGroup>
+              <div className='row justify-content-center mt-5'>
+                <Card className='mx-2 col-4 card-hover'>
+                  <Link to="/elecoverview" style={{ textDecoration: 'none', color: 'black' }}>
+                    <Card.Img variant="top" src={air} />
+                    <Card.Body>
+                      <Card.Title><b>Electrical</b></Card.Title>
+                      <Card.Text className='py-2'>
+                        Electrical Related tutorial will be found here
+                      </Card.Text>
+                    </Card.Body>
+                  </Link>   
+                </Card>
+                <Card className='mx-2 col-4 card-hover'>
+                  <Link to="/instoverview" style={{ textDecoration: 'none', color: 'black' }}>
+                    <Card.Img variant="top" src={unit} />
+                    <Card.Body>
+                      <Card.Title><b>Instruments</b></Card.Title>
+                      <Card.Text className='py-2'>
+                      Instruments Related tutorial will be found here
+                      </Card.Text>
+                    </Card.Body>
+                  </Link>   
+                </Card>
+              </div>
+           </div>
+           <Footer></Footer>
+    
         </div>
     );
 };
--- react-app/src/components/Home/Coursecategory.js
+++ react-app/src/components/Home/Coursecategory.js
@@ -3,46 +3,72 @@ import CardGroup from 'react-bootstrap/CardGroup';
 import engine from '../image/engine0.png';
 import airframe from '../image/airframe.png';
 import radio from '../image/radio.jpg';
+import air from '../image/aircraft.jpg';
+import unit from '../image/unit.jpg';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function Coursecategory() {
   return (
     <div className="container">
-    <CardGroup>
-      <Card className='mx-2'>
-      <Link to="/overview" style={{ textDecoration: 'none', color: 'black' }}>
-        <Card.Img variant="top" src={engine}/>
-        <Card.Body>
-          <Card.Title><b>Engine</b></Card.Title>
-          <Card.Text className='py-2'>
-            Engine Related tutorial will be found here
-          </Card.Text>
-        </Card.Body>
-        </Link>
-      </Card>
-      <Card className='mx-2'>
-      <Link to="/aircond" style={{ textDecoration: 'none', color: 'black' }}>
-        <Card.Img variant="top" src={airframe} />
-        <Card.Body>
-          <Card.Title><b>Air Frame</b></Card.Title>
-          <Card.Text className='py-2'>
-          Air Craft Related tutorial will be found here
-          </Card.Text>
-        </Card.Body>
-        </Link> 
-      </Card>
-      <Card className='mx-2'>
-      <Link to="/radoverview" style={{ textDecoration: 'none', color: 'black' }}>
-        <Card.Img variant="top" src={radio} />
-        <Card.Body>
-          <Card.Title><b>Radio</b></Card.Title>
-          <Card.Text className='py-2'>
-          Radio Related tutorial will be found here
-          </Card.Text>
-        </Card.Body>
-        </Link>   
-      </Card>
-    </CardGroup>
+      <CardGroup>
+        <Card className='mx-2 card-hover'>
+          <Link to="/overview" style={{ textDecoration: 'none', color: 'black' }}>
+            <Card.Img variant="top" src={engine}/>
+            <Card.Body>
+              <Card.Title><b>Engine</b></Card.Title>
+              <Card.Text className='py-2'>
+                Engine Related tutorial will be found here
+              </Card.Text>
+            </Card.Body>
+          </Link>
+        </Card>
+        <Card className='mx-2 card-hover'>
+          <Link to="/aircond" style={{ textDecoration: 'none', color: 'black' }}>
+            <Card.Img variant="top" src={airframe} />
+            <Card.Body>
+              <Card.Title><b>Air Frame</b></Card.Title>
+              <Card.Text className='py-2'>
+              Air Craft Related tutorial will be found here
+              </Card.Text>
+            </Card.Body>
+          </Link> 
+        </Card>
+        <Card className='mx-2 card-hover'>
+          <Link to="/radoverview" style={{ textDecoration: 'none', color: 'black' }}>
+            <Card.Img variant="top" src={radio} />
+            <Card.Body>
+              <Card.Title><b>Radio</b></Card.Title>
+              <Card.Text className='py-2'>
+              Radio Related tutorial will be found here
+              </Card.Text>
+            </Card.Body>
+          </Link>   
+        </Card>
+      </CardGroup>
+      <div className='row justify-content-center mt-5'>
+        <Card className='mx-2 col-4 card-hover'>
+          <Link to="/elecoverview" style={{ textDecoration: 'none', color: 'black' }}>
+            <Card.Img variant="top" src={air} />
+            <Card.Body>
+              <Card.Title><b>Electrical</b></Card.Title>
+              <Card.Text className='py-2'>
+                Electrical Related tutorial will be found here
+              </Card.Text>
+            </Card.Body>
+          </Link>   
+        </Card>
+        <Card className='mx-2 col-4 card-hover'>
+          <Link to="/instoverview" style={{ textDecoration: 'none', color: 'black' }}>
+            <Card.Img variant="top" src={unit} />
+            <Card.Body>
+              <Card.Title><b>Instruments</b></Card.Title>
+              <Card.Text className='py-2'>
+              Instruments Related tutorial will be found here
+              </Card.Text>
+            </Card.Body>
+          </Link>   
+        </Card>
+      </div>
     </div>
   );
 }
